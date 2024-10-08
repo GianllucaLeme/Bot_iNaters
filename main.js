fd930566dcb52928f9847072f802e06da9be3b4e
GianllucaLeme <gianllucadcl@gmail.com>
2024-10-08 18:45:58 -0300

    Add more commands

--- main.js
+++ main.js
@@ -22,22 +22,55 @@ client.on('qr', (qr) => {
 /*---Funcionalidades do bot ---*/
 
 
-let lista_comandos = ['/formigas', '/2', '/1'];
+let lista_comandos = ['/aranhas', '/aranhas2', '/formigas', '/1'];
 
 // Carrega o arquivo JSON
 const fs = require('fs');
 const c = JSON.parse(fs.readFileSync('contacts.json', 'utf-8'));
 
 async function Comandos(message) {
-    if (message.body === '/2'){
-        await client.sendMessage(message.from, `@${c.aranhas.melga}`, 
-            { mentions: [c.aranhas.melga + '@c.us'] });
+    if (message.body === '/aranhas'){
+        await client.sendMessage(message.from, 
+            `@${c.aranhas.celio}, @${c.aranhas.fernando}, @${c.aranhas.gianlluca}, @${c.aranhas.isaac}, @${c.aranhas.jean}, @${c.aranhas.leonardo}, @${c.aranhas.lucas_gusso}, @${c.aranhas.ryan}, @${c.aranhas.victor}`, 
+    
+            { mentions: [
+                c.aranhas.celio + '@c.us', 
+                c.aranhas.fernando + '@c.us', 
+                c.aranhas.gianlluca + '@c.us',
+                c.aranhas.isaac + '@c.us',
+                c.aranhas.jean + '@c.us',
+                c.aranhas.leonardo + '@c.us',
+                c.aranhas.lucas_gusso + '@c.us',
+                c.aranhas.ryan + '@c.us',
+                c.aranhas.victor + '@c.us'
+            ]});
+    }
+
+    if (message.body === '/aranhas2'){
+        await client.sendMessage(message.from, 
+            `@${c.aranhas2.adolfo}, @${c.aranhas2.claudia}, @${c.aranhas2.dayvson}, @${c.aranhas2.gabriel_costa}, @${c.aranhas2.michelotto}, @${c.aranhas2.pedro_martins}, @${c.aranhas2.piva}`, 
+            
+            { mentions: [
+                c.aranhas2.adolfo + '@c.us',
+                c.aranhas2.claudia + '@c.us',
+                c.aranhas2.dayvson + '@c.us',
+                c.aranhas2.gabriel_costa + '@c.us',
+                c.aranhas2.michelotto + '@c.us',
+                c.aranhas2.pedro_martins + '@c.us',
+                c.aranhas2.piva + '@c.us'
+            ]});
     }
 
     if (message.body === '/formigas'){
-        await client.sendMessage(message.from, `@${c.borboletas.andre}, @${c.formigas.maycon}, @${c.borboletas.fischer}, @${c.enrico}`, 
-            {mentions: [c.borboletas.andre + '@c.us', c.formigas.maycon + '@c.us', 
-                c.borboletas.fischer + '@c.us', c.enrico + '@c.us']});
+        await client.sendMessage(message.from, `@${c.formigas.felipe_santos}, @${c.formigas.gabriel_rogerio}, @${c.formigas.guilherme_cardoso}, @${c.formigas.joao_paulo}, @${c.formigas.maycon}`, 
+            
+            {mentions: [
+                c.formigas.felipe_santos + '@c.us',
+                c.formigas.gabriel_rogerio + '@c.us',
+                c.formigas.guilherme_cardoso + '@c.us',
+                c.formigas.joao_paulo + '@c.us',
+                c.formigas.maycon + '@c.us',
+            ]});
     }
 
     if (message.body === '/1'){

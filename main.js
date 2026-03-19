84361c8409603d93b91028aebc1df2c66bee3a62
GianllucaLeme <gianllucadcl@gmail.com>
2026-03-19 01:15:17 -0300

    Update /help, /admin, /milicia, /sobre, /aranhas, /aves, /escorpioes, /formiga, /louva, /mariposas, /moscas, /opilioes

---
+++ main.js
@@ -0,0 +1,483 @@
+/*--- Sincronização do bot com o WhatsApp ---*/
+
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth, MessageMedia} = require('whatsapp-web.js');
+
+// Create a new client instance
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Client is ready!');
+});
+
+// When the client received QR-Code
+client.on('qr', (qr) => {
+    qrcode.generate(qr, {small: true});
+});
+
+
+/*--- Funcionalidades do bot ---*/
+
+
+let lista_comandos = [
+    '/help', '/admin', '/bicho', '/milicia', '/sobre', '/tirar_nome', 
+    
+    '/aranhas', '/abelhas', '/aves', '/besouros', 
+    '/borboletas', '/cigarras', '/diplopoda', '/escorpioes', '/escorpiões', 
+    '/formigas', '/geoplanaria', '/geoplanária', '/louva', '/mantis', '/mariposas', 
+    '/moscas', '/opilioes','/opiliões', '/percevejos', '/phasma', '/plantas', 
+    
+    '/stop', '/all'];
+
+let lista_easter = ['/bloisinho', '/cladoFSM', '/cladofsm', 
+    '/cladoPCM', '/cladopcm', '/mateiro', '/meriva', '/vermoidea'];
+
+// Carrega o arquivo JSON
+const fs = require('fs');
+const c = JSON.parse(fs.readFileSync('contacts.json', 'utf-8'));
+
+let lista_admins = [c.louva.cesar, c.phasma.edgar, c.mariposas.fischer, c.enrico, c.besouros.lorenne, c.aranhas.jean, c.carlos, c.aranhas.gianlluca];
+
+// Função que determina a chance do usuário ser marcado
+async function chanceUsuario(usuario, chance) {
+    if (Math.random() < chance) {
+        return usuario;
+    } else {
+        return null;
+    }
+}
+
+// Função que adiciona o usuário para ser propriamente mencionado na mensagem
+async function mencionarUsuario(lista, pessoas, usuario, chance) {
+    const marcacao = await chanceUsuario(usuario, chance);
+    if(marcacao){
+        lista.push(marcacao + '@c.us');
+        return pessoas + `, @${marcacao}`;
+    }else{
+        return pessoas;
+    }
+}
+
+// Função para embaralhar a lista de contatos
+async function embaralharContatos(contatos) {
+    for (let i = contatos.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [contatos[i], contatos[j]] = [contatos[j], contatos[i]];  // Troca os elementos
+    }
+    return contatos;
+}
+
+// Função principal que rege todos os comandos
+async function Comandos(message) {
+    /*--- Comandos Ajuda ---*/
+    
+    if (message.body === '/help') {
+        const usuario_duvida = message.author || message.from;
+        
+        const comandosAjuda = [
+            { comando: '/help', descricao: 'Mostra os comandos atuais do bot;' },
+            { comando: '/admin', descricao: 'Marca dois admins aleatórios;' },
+            { comando: '/bicho', descricao: 'Usar quando não souber quem marcar;' },
+            { comando: '/milicia', descricao: 'Usar quando precisar de ajuda para virar IDs no iNat;' },
+            { comando: '/sobre', descricao: 'Mostra informações sobre o bot;'},
+            { comando: '/tirar_nome', descricao: 'Abre um requerimento para retirar seu nome das marcações.'}
+        ];
+
+        let comandos_removidos = ['/help', '/admin', '/bicho', '/milicia', '/sobre', '/tirar_nome', '/stop', '/all', '/escorpiões', '/geoplanária', '/opiliões'];
+        let comandosPrincipais = lista_comandos.filter(comando => !comandos_removidos.includes(comando));
+
+        let mensagem = `Olá! Esses são os comandos disponíveis até o momento:\n\n`;
+        
+        mensagem += `> Comandos de Ajuda\n`;
+        mensagem += comandosAjuda.map(cmd_help => `* \`${cmd_help.comando}\` - ${cmd_help.descricao}`).join('\n') + '\n\n';
+
+        mensagem += `> Comandos Principais - marcam os membros que trabalham em seus respectivos grupos\n`;
+        mensagem += comandosPrincipais.map(cmd_main => {
+            if (cmd_main === '/diplopoda') {
+                return `* \`${cmd_main}\` - piolho-de-cobra`;
+            } else if (cmd_main === '/escorpioes') {
+                return `* \`${cmd_main}\` ou \`/escorpiões\``;
+            } else if (cmd_main === '/geoplanaria') {
+                return `* \`${cmd_main}\` ou \`/geoplanária\` - planária terrestre`;          
+            } else if (cmd_main === '/louva') {
+                return `* \`${cmd_main}\` ou \`/mantis\` - louva-a-deus`;
+            } else if (cmd_main === '/opilioes'){
+                return `* \`${cmd_main}\` ou \`/opiliões\``;
+            } else if (cmd_main === '/phasma'){
+                return `* \`${cmd_main}\` - bicho-pau`;
+            } else {
+                return `* \`${cmd_main}\``;
+            }
+        }).join('\n');       
+
+        await client.sendMessage(usuario_duvida, mensagem);
+    }
+
+    if (message.body === '/admin'){
+        lista_admins_filtered = lista_admins.filter(admin => admin !== c.aranhas.gianlluca)
+        lista_admins_filtered = (await embaralharContatos(lista_admins_filtered)).slice(0, 2)
+
+        let lista = lista_admins_filtered.map(user => `${user}@c.us`);
+        let pessoas = `@${lista_admins_filtered.join(', @')}`;
+        
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (message.body === '/bicho'){
+        const bicho = [c.enrico, c.aranhas.celio, c.phasma.edgar];
+        let lista = bicho.map(user => `${user}@c.us`);
+        let pessoas = `@${bicho.join(', @')}`;
+        
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (message.body === '/milicia'){
+        let ajudantes = [c.aranhas.celio, c.aranhas.gianlluca, c.mariposas.fischer, 
+            c.mariposas.luis_eduardo, c.formigas.davi, c.formigas.gabriel_rogerio, 
+            c.formigas.maycon, c.phasma.edgar, c.enrico, c.shiva, c.jose_valerio];
+        
+        ajudantes = (await embaralharContatos(ajudantes)).slice(0, 5)
+
+        let lista = ajudantes.map(user => `${user}@c.us`);
+        let pessoas = `@${ajudantes.join(', @')}`;
+        
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (message.body === '/sobre') {
+        const usuario_curioso = message.author || message.from;
+
+        let mensagem = 'O \`iMark\` foi criado para facilitar o processo de identificação de animais, plantas, fungos etc., permitindo a marcação automática de membros especializados em seus grupos taxonômicos.\n\n';
+
+        mensagem += 'Com essa funcionalidade, elimina-se a necessidade de saber exatamente quem marcar, sendo especialmente útil para quem é novo no grupo. Além disso, esse bot promove uma interação melhor e mais dinâmica entre os membros mais antigos e novos do grupo.\n\n';
+        
+        mensagem += 'Sugestões mandar no privado do autor! 👇\n\n'
+        
+        mensagem += `Desenvolvedor: @${c.aranhas.gianlluca}\n`;
+        mensagem += 'Versão atual: \`\`\`1.0.0\`\`\`\n';
+        mensagem += 'GitHub: https://github.com/GianllucaLeme/Bot_iNaters';
+
+        await client.sendMessage(usuario_curioso, mensagem, {mentions: c.aranhas.gianlluca + '@c.us'});
+    }
+
+    if (message.body === '/tirar_nome') {
+        let usuario_tirar = await message.getContact();
+
+        await client.sendMessage(message.from, '> Requerimento enviado.');
+        
+        setTimeout(async () => {
+            await client.sendMessage(c.aranhas.gianlluca + '@c.us', `O @${usuario_tirar.id.user} quer retirar a marcação!`, {mentions: usuario_tirar.id.user + '@c.us'});
+        }, 3000);
+    }
+    
+    /*--- Comandos Principais ---*/
+
+    if (message.body === '/abelhas') {
+        const abelhas = [c.abelhas.bruno_aranda, c.abelhas.beatriz, c.abelhas.abelhero_misterioso];
+        let lista = abelhas.map(user => `${user}@c.us`);
+        let pessoas = `@${abelhas.join(', @')}`;
+    
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, 0.3);
+
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (message.body === '/aranhas'){
+        let aranhas = [c.aranhas.adolfo, c.aranhas.alfredo, c.aranhas.claudia, c.aranhas.dayvson, 
+            c.aranhas.fernando, c.aranhas.gabriel_costa, c.aranhas.isaac, c.aranhas.lucas_gusso, 
+            c.aranhas.michelotto, c.aranhas.pedro_martins, c.aranhas.victor];
+        
+        let prioridade = [c.aranhas.celio, c.aranhas.gianlluca, c.aranhas.jean, c.aranhas.ryan];
+
+        aranhas = (await embaralharContatos(aranhas)).slice(0, 3);
+
+        let lista = aranhas.map(user => `${user}@c.us`);
+        let pessoas = `@${aranhas.join(', @')}`;
+        
+        for (let i = 0; i < prioridade.length; i++) {
+            pessoas = await mencionarUsuario(lista, pessoas, prioridade[i], 0.633975);
+        }
+
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, 0.5);
+
+        await client.sendMessage(message.from, pessoas, { mentions: lista});
+    }
+
+    if (message.body === '/aves') {
+        let aves = [c.plantas.edvandro, c.aves.jose_valerio, c.aves.matheus_santos, 
+            c.aves.miguel_malta, c.aves.henrique_stranz, c.aves.ruan];
+       
+        aves = (await embaralharContatos(aves)).slice(0, 3);
+        
+        let lista = aves.map(user => `${user}@c.us`);
+        let pessoas = `@${aves.join(', @')}`;
+    
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, 0.95);
+        pessoas = await mencionarUsuario(lista, pessoas, c.aves.victor_aves, 0.3)
+        
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (message.body === '/besouros') {
+        const besouros = [c.besouros.lorenne, c.besouros.vincenzo, c.besouros.bruno_begha];
+        let lista = besouros.map(user => `${user}@c.us`);
+        let pessoas = `@${besouros.join(', @')}`;
+    
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, 0.336);
+        
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (message.body === '/borboletas'){
+        const borboletas =  [c.borboletas.andre_nog, c.borboletas.pedro_souza, 
+            c.borboletas.rafaela, c.borboletas.tiago];
+
+        let lista = borboletas.map(user => `${user}@c.us`);
+        let pessoas = `@${borboletas.join(', @')}`;
+        
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, 0.5);
+        pessoas = await mencionarUsuario(lista, pessoas, c.mariposas.fischer, 0.15);
+        
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (message.body === '/cigarras') {
+        const cigarras = [c.cigarras.bruno];
+        let lista = cigarras.map(user => `${user}@c.us`);
+        let pessoas = `@${cigarras.join(', @')}`;
+    
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, 0.2);
+        pessoas = await mencionarUsuario(lista, pessoas, c.mariposas.fischer, 0.15);
+
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (message.body === '/diplopoda') {
+        const diplopoda = [c.diplopoda.rodrigo_bouzan];
+        let lista = diplopoda.map(user => `${user}@c.us`);
+        let pessoas = `@${diplopoda.join(', @')}`;
+    
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, 0.5);
+
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (['/escorpioes', '/escorpiões'].includes(message.body)){
+        let escorpioes = [c.aranhas.adolfo, c.aranhas.fernando, c.aranhas.gianlluca, c.aranhas.lucas_gusso, c.aranhas.pedro_martins];
+        let prioridade = [c.aranhas.celio, c.aranhas.jean, c.aranhas.michelotto];
+
+        escorpioes = (await embaralharContatos(escorpioes)).slice(0, 3);
+
+        let lista = escorpioes.map(user => `${user}@c.us`);
+        let pessoas = `@${escorpioes.join(', @')}`;
+        
+        for (let i = 0; i < prioridade.length; i++) {
+            pessoas = await mencionarUsuario(lista, pessoas, prioridade[i], 0.2);
+        }
+
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, 0.5);
+
+        await client.sendMessage(message.from, pessoas, { mentions: lista});
+    }
+
+    if (message.body === '/formigas'){
+        let prioridade = [c.formigas.davi, c.formigas.felipe_santos, c.formigas.gabriel_rogerio, 
+                    c.formigas.joao_paulo, c.formigas.maycon];
+
+        let lista = prioridade.map(user => `${user}@c.us`);
+        let pessoas = `@${prioridade.join(', @')}`;
+
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, 0.5);
+        pessoas = await mencionarUsuario(lista, pessoas, c.formigas.didobola, 0.05);
+        pessoas = await mencionarUsuario(lista, pessoas, c.formigas.vankan, 0.03);
+        pessoas = await mencionarUsuario(lista, pessoas, c.formigas.diego, 0.05);
+
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (['/geoplanaria', '/geoplanária'].includes(message.body)) {
+        const geoplanaria = [c.geoplanaria.piter];
+        let lista = geoplanaria.map(user => `${user}@c.us`);
+        let pessoas = `@${geoplanaria.join(', @')}`;
+    
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, 0.2);
+
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (['/louva', '/mantis'].includes(message.body)) {
+        const louva = [c.louva.cesar, c.louva.gabriel_gomes, 
+            c.louva.leo, c.louva.lorena, c.louva.savio];
+
+        let lista = louva.map(user => `${user}@c.us`);
+        let pessoas = `@${louva.join(', @')}`;
+    
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, 0.02);
+        
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (message.body === '/mariposas'){
+        const mariposas = [c.mariposas.fischer, c.mariposas.laila, c.mariposas.luis_eduardo,
+            c.mariposas.miguel, c.mariposas.nicolas, c.phasma.pedro_alvaro];
+        
+        let lista = mariposas.map(user => `${user}@c.us`);
+        let pessoas = `@${mariposas.join(', @')}`;
+        
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, 0.5);
+        
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (message.body === '/moscas') {
+        const moscas = [c.moscas.matheus, c.moscas.rodrigo, c.moscas.lais, c.moscas.luan];
+
+        let lista = moscas.map(user => `${user}@c.us`);
+        let pessoas = `@${moscas.join(', @')}`;
+    
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, 0.1);
+        
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (['/opilioes', '/opiliões'].includes(message.body)) {
+        const opilioes = [c.opilioes.lohan, c.opilioes.luis_cla, c.opilioes.thales];
+        
+        let lista = opilioes.map(user => `${user}@c.us`);
+        let pessoas = `@${opilioes.join(', @')}`;
+    
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, 0.2);
+        
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (message.body === '/percevejos') {
+        const percevejos = [c.percevejos.guilherme_lopez];
+        let lista = percevejos.map(user => `${user}@c.us`);
+        let pessoas = `@${percevejos.join(', @')}`;
+    
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, 0.8);
+
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (message.body === '/phasma') {
+        const phasma = [c.phasma.edgar, c.phasma.pedro_alvaro, c.phasma.pedro_sisnando, c.phasma.phil];
+        let lista = phasma.map(user => `${user}@c.us`);
+        let pessoas = `@${phasma.join(', @')}`;
+    
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, 0.01);
+        
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (message.body === '/plantas') {
+        const plantas = [c.plantas.edvandro];
+        let lista = plantas.map(user => `${user}@c.us`);
+        let pessoas = `@${plantas.join(', @')}`;
+        
+        pessoas = await mencionarUsuario(lista, pessoas, c.mariposas.fischer, 0.1);
+        pessoas = await mencionarUsuario(lista, pessoas, c.enrico, -0.336);
+        
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    /*--- Comandos Admin ---*/
+
+    if (message.body === '/stop') {
+        let admin = await message.getContact();
+
+        if (lista_admins.includes(admin.id.user)) {
+            await client.sendMessage(message.from, '> Bot desligando...');
+            
+            // Espera 1 s para que a mensagem seja enviada no WhatsApp
+            setTimeout(async () => {
+                await client.destroy();
+            }, 1000);
+        }else{
+            await client.sendMessage(message.from, '> Você não tem autorização para utilizar esse comando.');
+        }
+    }
+    
+    if (message.body === '/all') {
+        let admin = await message.getContact();
+
+        if (lista_admins.includes(admin.id.user)) {
+            const chat = await message.getChat();
+            let mentions = [];
+        
+            for (let participantes of chat.participants) {
+                mentions.push(`${participantes.id.user}@c.us`);
+            }
+        
+            await chat.sendMessage('> Marcando todo mundo...', {mentions});
+        }else{
+            await client.sendMessage(message.from, '> Você não tem autorização para utilizar esse comando.');
+        }
+    }
+
+    /*--- Comandos Easter Eggs ---*/
+
+    if (message.body === '/bloisinho'){
+        let random_blois = Math.floor(Math.random()*4);
+        const media = MessageMedia.fromFilePath(`./pictures/bloisinhos/blois${random_blois}.png`);
+        await client.sendMessage(message.from, media, { sendMediaAsSticker: true });
+    }
+
+    if (['/cladoFSM', '/cladofsm'].includes(message.body)) {
+        const clado_fsm = [c.mariposas.fischer, c.shiva, c.formigas.maycon];
+        let lista = clado_fsm.map(user => `${user}@c.us`);
+        let pessoas = `@${clado_fsm.join(', @')}`;
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+
+    if (message.body === '/mateiro'){
+        let random_malta = Math.floor(Math.random()*9);
+        const media = MessageMedia.fromFilePath(`./pictures/mateiros/mateiro${random_malta}.png`);
+        await client.sendMessage(message.from, media, { sendMediaAsSticker: true });
+    }
+
+    if (message.body === '/meriva'){
+        let random_meriva = Math.floor(Math.random()*11);
+        const media = MessageMedia.fromFilePath(`./pictures/merivas/meriva${random_meriva}.png`);
+        await client.sendMessage(message.from, media);
+    }
+
+    if (message.body === '/vermoidea'){
+        const vermoidea = [c.mariposas.fischer, c.shiva, c.formigas.maycon, c.aranhas.gabriel_costa, c.aranhas.adolfo];
+        let lista = vermoidea.map(user => `${user}@c.us`);
+        let pessoas = `@${vermoidea.join(', @')}`;
+        await client.sendMessage(message.from, pessoas, {mentions: lista});
+    }
+}
+
+// Spam handling
+let lista_spam = [];
+let flag_spam = 0;
+
+// Bot, em loop, lendo as mensagens
+client.on('message_create', async message => {
+    let usuario = await message.getContact();
+    
+    // Spam handling antes de detectar os comandos
+    if ([...lista_comandos, ...lista_easter].includes(message.body)) {
+        let msg1 = message.timestamp;
+        lista_spam[flag_spam] = msg1;
+
+        flag_spam++;
+
+        if(flag_spam == 2){
+            flag_spam = 0;
+        }
+
+        if(!(Math.abs(lista_spam[1] - lista_spam[0]) < 3)){
+            await Comandos(message);
+        }
+    }
+});
+
+// Ligar o bot
+client.initialize();
